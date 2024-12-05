3aeed946bfb5611fbca8aa2c3f43456189731f4b
Kacper Pietkun <kacper.pietkun00@gmail.com>
2024-12-05 17:51:37 +0100

    log D_x and D_G_x during GAN training

--- train_gan_model.py
+++ train_gan_model.py
@@ -13,6 +13,11 @@ from utils import ModelSaver, TensorDatasetWithAugmentations
 from gan_model import GAN
 import matplotlib.pyplot as plt
 
+#TODO: add MIFID(Memorization Informed Frechet Inception Distance) metric
+# sieć powinna być cała ConvNet
+# Tanh zamiast sigmoida w generatorze, i zdjęcia trzeba przeskalować do [-1, 1]
+# weight initialization - a zero-centered Normal distribution with standard deviation 0.02
+
 parser = ArgumentParser("Training script for GAN")
 
 parser.add_argument("--dataset-path", type=str, required=True,
@@ -41,6 +46,9 @@ parser.add_argument("--weight-decay", type=float, default=0.0,
 
 parser.add_argument("--momentum", type=float, default=0.5,
                     help="momentum passed to optimizer")
+
+parser.add_argument("--noise-dimension", type=int, default=100,
+                    help="Number of training epochs")
 
 parser.add_argument("--batch-size", type=int, default=128,
                     help="training batch size")
@@ -128,6 +136,15 @@ def plot_losses(args, history):
     plt.legend()
     plt.savefig(f"{args.save_path}/train_losses.jpg")
 
+    train_D_x = [epoch_dict.get("train_D_x") for epoch_dict in history]
+    train_D_G_x = [epoch_dict.get("train_D_G_x") for epoch_dict in history]
+    plt.figure(figsize=(14, 8))
+    plt.plot(train_D_x, color="r", label=" train_D_x")
+    plt.plot(train_D_G_x, color="b", label="train_D_G_x")
+    plt.title("Train D_x/D_G_x over epochs")
+    plt.legend()
+    plt.savefig(f"{args.save_path}/train_d_x.jpg")
+
     val_discriminator_losses = [epoch_dict.get("val_discriminator_loss") for epoch_dict in history]
     val_generator_losses = [epoch_dict.get("val_generator_loss") for epoch_dict in history]
     plt.figure(figsize=(14, 8))
@@ -137,10 +154,19 @@ def plot_losses(args, history):
     plt.legend()
     plt.savefig(f"{args.save_path}/Validation_losses.jpg")
 
+    val_D_x = [epoch_dict.get("val_D_x") for epoch_dict in history]
+    val_D_G_x = [epoch_dict.get("val_D_G_x") for epoch_dict in history]
+    plt.figure(figsize=(14, 8))
+    plt.plot(val_D_x, color="r", label=" val_D_x")
+    plt.plot(val_D_G_x, color="b", label="val_D_G_x")
+    plt.title("Validation D_x/D_G_x over epochs")
+    plt.legend()
+    plt.savefig(f"{args.save_path}/val_d_x.jpg")
+
 
 def train(args, model, train_dataloader, val_dataloader, optimizers, device, model_saver):
     discriminator_optimizer, generator_optimizer = optimizers
-    loss_fun = nn.BCEWithLogitsLoss()
+    loss_fun = nn.BCELoss()
     history = []
     best_val_loss = math.inf
     visualization_train_path = os.path.join(args.save_path, "visualization_train")
@@ -152,6 +178,7 @@ def train(args, model, train_dataloader, val_dataloader, optimizers, device, mod
     for epoch in range(args.epochs):
         model.train()
         train_discriminator_loss, train_generator_loss = 0, 0
+        train_D_x, train_D_G_x = 0, 0
         
         for real_images in tqdm(train_dataloader):
             bs = real_images.shape[0]
@@ -159,13 +186,13 @@ def train(args, model, train_dataloader, val_dataloader, optimizers, device, mod
             fake_images = model.generator.generate(bs)
             
             discriminator_optimizer.zero_grad()
-
             out_fake = model.discriminator(fake_images.detach())
             discriminator_loss_fake = loss_fun(out_fake, fake_labels[:bs])
 
             out_real = model.discriminator(real_images)
             discriminator_loss_real = loss_fun(out_real, real_labels[:bs])
-
+            train_D_x += out_real.sum()
+            train_D_G_x += out_fake.sum()
             discriminator_loss = discriminator_loss_real + discriminator_loss_fake
             discriminator_loss.backward()
             discriminator_optimizer.step()
@@ -174,6 +201,7 @@ def train(args, model, train_dataloader, val_dataloader, optimizers, device, mod
             generator_optimizer.zero_grad()
             out_generator = model.discriminator(fake_images)
             generator_loss = loss_fun(out_generator, real_labels[:bs])
+            train_D_G_x += out_generator.sum()
             generator_loss.backward()
             generator_optimizer.step()
             train_generator_loss += generator_loss.item() * bs
@@ -182,6 +210,7 @@ def train(args, model, train_dataloader, val_dataloader, optimizers, device, mod
 
         model.eval()
         val_discriminator_loss, val_generator_loss = 0, 0
+        val_D_x, val_D_G_x = 0, 0
         with torch.no_grad():
             for real_images in tqdm(val_dataloader):
                 bs = real_images.shape[0]
@@ -194,24 +223,36 @@ def train(args, model, train_dataloader, val_dataloader, optimizers, device, mod
                 out_real = model.discriminator(real_images)
                 discriminator_loss_real = loss_fun(out_real, real_labels[:bs])
 
+                val_D_x += out_real.sum()
+                val_D_G_x += out_fake.sum()
                 discriminator_loss = discriminator_loss_real + discriminator_loss_fake
                 val_discriminator_loss += discriminator_loss.item() * bs
 
                 out_generator = model.discriminator(fake_images)
                 generator_loss = loss_fun(out_generator, real_labels[:bs])
+                val_D_G_x += out_generator.sum()
                 val_generator_loss += generator_loss.item() * bs
         save_visualization_outputs(fake_images[:10].cpu(), visualization_val_path, epoch)
         
         train_discriminator_loss /= len(train_dataloader.sampler)
         train_generator_loss /= len(train_dataloader.sampler)
+        train_D_x /= len(train_dataloader.sampler)
+        train_D_G_x /= 2 * len(train_dataloader.sampler)
         val_discriminator_loss /= len(val_dataloader.sampler)
         val_generator_loss /= len(val_dataloader.sampler)
+        val_D_x /= len(val_dataloader.sampler)
+        val_D_G_x /= 2* len(val_dataloader.sampler)
+
         history.append({
             "epoch": epoch,
             "train_discriminator_loss": train_discriminator_loss,
             "train_generator_loss": train_generator_loss,
+            "train_D_x": train_D_x.item(),
+            "train_D_G_x": train_D_G_x.item(),
             "val_discriminator_loss": val_discriminator_loss,
             "val_generator_loss": val_generator_loss,
+            "val_D_x": val_D_x.item(),
+            "val_D_G_x": val_D_G_x.item(),
         })
         model_saver.save(model.state_dict(), val_generator_loss)
         best_val_loss = min(best_val_loss, val_generator_loss)
@@ -219,15 +260,23 @@ def train(args, model, train_dataloader, val_dataloader, optimizers, device, mod
             args.trial.report(val_generator_loss, epoch) 
             if args.trial.should_prune():
                 raise optuna.exceptions.TrialPruned()
-        print('Epoch: {} Train Loss: {:.4f}, {:.4f} Validation Loss: {:.4f}, {:.4f} '.format(epoch, train_generator_loss, train_discriminator_loss,
-                                                                                              train_generator_loss, train_discriminator_loss))
+        print('Epoch: {} Train Loss: {:.4f}, {:.4f}, {:.4f}, {:.4f} \
+               Validation Loss: {:.4f}, {:.4f}, {:.4f}, {:.4f} '.format(epoch,
+                                                                        train_generator_loss,
+                                                                        train_discriminator_loss,
+                                                                        train_D_x,
+                                                                        train_D_G_x,
+                                                                        train_generator_loss,
+                                                                        train_discriminator_loss,
+                                                                        val_D_x,
+                                                                        val_D_G_x))
     return history, best_val_loss
 
 
 def main(args):
     device = get_device(args.gpu)
 
-    gan = GAN(3, device).to(device)
+    gan = GAN(3, args.noise_dimension, device).to(device)
     augmentations = get_augmentations(args)
     train_dataloader, val_dataloader, _ = create_dataloaders(args, augmentations)
     discriminator_optimizer = create_optimizer(gan.discriminator, args)
